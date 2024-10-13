774cc18b6be8002d1c50b87197cc5a02d934eec1
Danny <danilbliz0201@gmail.com>
2024-10-13 11:46:49 +0300

    chore: update README.md

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,21 @@
-import antfu from '@antfu/eslint-config'
+import antfu from '@antfu/eslint-config';
 
 export default antfu({
   formatters: true,
   stylistic: {
     indent: 2,
     semi: true,
-  }
+  },
 }, {
   rules: {
     'no-console': 'warn',
     'antfu/no-top-level-await': 'off',
-    'perfectionist/sort-imports': ["error", {
-      internalPattern: ["~/**"],
+    'perfectionist/sort-imports': ['error', {
+      internalPattern: ['~/**'],
     }],
     'unicorn/filename-case': ['error', {
       case: 'kebabCase',
-    }]
-  }
-})
+      ignore: ['^.*\.md$'],
+    }],
+  },
+});

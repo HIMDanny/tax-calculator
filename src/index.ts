e972f2e4d1efd24c0977d184ae6f9c8463fdd90c
Danny <danilbliz0201@gmail.com>
2024-10-16 20:57:42 +0300

    chore: add terminal messages to inform user about useful links

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,13 @@ import env from './env';
 const PORT = env.PORT;
 
 // eslint-disable-next-line no-console
-console.log(`Running on http://localhost:${PORT}`);
+console.log(`Running on http://localhost:${PORT}/api`);
+
+// eslint-disable-next-line no-console
+console.log(`Documentation: http://localhost:${PORT}/doc`);
+
+// eslint-disable-next-line no-console
+console.log(`Documentation: http://localhost:${PORT}/reference`);
 
 serve({
   fetch: app.fetch,
